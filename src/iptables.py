fa980bd8806d4b4971442df6711d56ecc964e1f6
xiwhe <hxw1000@hotmail.com>
2018-07-15 16:16:51 +0800

    remove iptc, use systemd instead

--- src/iptables.py
+++ src/iptables.py
@@ -3,15 +3,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jul 12 10:18:03 2018
+
 @author: xiwhe
 """
 
 
-import iptc
 from time import sleep
 from datetime import datetime
 import os
 import calendar
+from threading import Timer
 
 
 TRH_P_M = 16*1000*1000*1000
@@ -21,13 +22,12 @@ TRH_P_M = 16*1000*1000*1000
 class IPtableManager:
 
     _ports = list()
-    _cnt_setted = False
     _used_traffic = 0
 
     def __init__(self, supervise_ports):
 
         self._ports = supervise_ports
-
+ 
         mothrange = calendar.monthrange(datetime.today().year,datetime.today().month)[1]
 
         clear_all_rules = "iptables -F"
@@ -36,6 +36,7 @@ class IPtableManager:
 
 #        with open("/tmp/ss_used", "rw+") as F:
 #            self._used_traffic = f.read()
+#            print "used: ", self._used_traffic
 
         traffic_limit = (TRH_P_M - self._used_traffic)/mothrange/self._ports.__len__()
         
@@ -49,46 +50,19 @@ class IPtableManager:
 
         pass
 
+    def _reset_TRH_limit(self):
+        print "start clear limitations"
+        clear_accept =  "iptables -Z"
+        os.system(clear_accept)
+        timer = Timer(30.0, self._reset_TRH_limit)
+        timer.start()
+        
+
     def start_supervising(self):
-        while True:
-            if datetime.today().hour is 23 and self.cnt_setted is False:
-                print "start clear limitations"
-                table = iptc.Table(iptc.Table.FILTER)
-                chain = iptc.Chain(table, 'OUTPUT')
-                chain.zero_counters()
-                self.cnt_setted = True
-            elif datetime.today().hour is 22 and self.cnt_setted is True:
-                print "cleared limitations"
-                self.cnt_setted = False
-            else:
-                print "wait for clear limitations"
-            sleep(60*50)
-
-
-def test():
-    table = iptc.Table(iptc.Table.FILTER)
-    for chain in table.chains:
-        print "======================="
-        print "Chain ", chain.name
-        for rule in chain.rules:
-            print "Rule", "proto:", rule.protocol, "src:", rule.src, "dst:", \
-                  rule.dst, "in:", rule.in_interface, "out:", rule.out_interface,
-            print "Matches:",
-            for match in rule.matches:
-                print match.name,
-            print "Target:",
-            print rule.target.name
-    print "======================="
-
-    table = iptc.Table(iptc.Table.FILTER)
-    chain0 = iptc.Chain(table, 'OUTPUT')
-    for rule in chain0.rules:
-        for match in rule.matches:
-            (packets, bytes) = rule.get_counters()
-            print packets, bytes, match.name, match.sport
+        _reset_TRH_limit()
 
 
 if __name__ == "__main__":
-    manager = IPtableManager([50011,50022])
+    manager = IPtableManager([50002])
     manager.start_supervising()
     pass
